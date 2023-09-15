78e636a64471a825ef638eed84c5eb2f8af73d6b
paper-pub-cit <145041483+paper-pub-cit@users.noreply.github.com>
2023-09-15 15:19:36 +0800

    Update globe.js

--- globe.js
+++ globe.js
@@ -107,7 +107,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture('world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture('earth_map.jpg');
 
     material = new THREE.ShaderMaterial({
 
